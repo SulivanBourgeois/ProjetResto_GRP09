d79d196196e65229a7a1b97ee1da7e65bc5f5de6
edouard <ecornet@la-joliverie.com>
2023-03-13 17:07:56 +0100

    creation de la plupart des visuels + changment de contact par resa

--- app/src/main/java/com/example/projetresto/modele/metier/typesresto.java
+++ app/src/main/java/com/example/projetresto/modele/metier/typesresto.java
@@ -1,4 +1,18 @@
 package com.example.projetresto.modele.metier;
 
 public class typesresto {
+
+    public String getTypeR() {
+        return typeR;
+    }
+
+    public void setTypeR(String typeR) {
+        this.typeR = typeR;
+    }
+
+    public typesresto(String typeR) {
+        this.typeR = typeR;
+    }
+
+    protected String typeR;
 }
